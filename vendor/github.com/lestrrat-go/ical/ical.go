99d53649fca3aa12d43e809d1a4d190583b0ea19
Sean Liao <seankhliao@gmail.com>
2020-04-25 15:18:58 +0200

    remove vendor

--- vendor/github.com/lestrrat-go/ical/ical.go
+++
@@ -1,9 +0,0 @@
-//go:generate go run internal/cmd/gentypes/gentypes.go definitions.json
-
-package ical
-
-import (
-	bufferpool "github.com/lestrrat-go/bufferpool"
-)
-
-var bufferPool = bufferpool.New()
